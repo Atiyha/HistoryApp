f9be696bb3c6ac2e8b64a892c4ba6831b260f250
Atiyha <ST10440871@vcconnect.edu.za>
2024-04-02 22:35:06 +0200

    Display View (Activity Main)

---
+++ app/src/main/java/com/example/thehistoryapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.thehistoryapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
