3cb5c5376b5bea209348c9b056ea35375ce3b1ad
Atiyha <ST10440871@vcconnect.edu.za>
2024-04-02 22:43:52 +0200

    Private Function for app reset

--- app/src/main/java/com/example/thehistoryapp/MainActivity.kt
+++ app/src/main/java/com/example/thehistoryapp/MainActivity.kt
@@ -11,6 +11,12 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private fun resetApp(){ //declaration of a private function
+        val intent = Intent(this, MainActivity :: class.java) //creating an intent object
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK) //adding flags to intent
+        startActivity(intent) //start activity specified in intent
+        finish() //finishes the current activity
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
